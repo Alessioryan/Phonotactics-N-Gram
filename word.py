98092253b07e4a105344a4c4cfc34e9abff072fa
Tosolini <tosolini@uw.edu>
2022-10-26 00:56:44 -0700

    fix word.py decompose_word method, to do: test corner cases

--- word.py
+++ word.py
@@ -11,27 +11,37 @@ def decompose_word(word):
     # This code would vary language to language. For now, let's assume always syllables
     # with monophthong vowels, with onsets and codas being at most one consonant but potentially null
     # If there is a consonant between two vowels, it is assumed to be a onset by default
-
+    word += "XXX"
     syllables = []
-
-    left_pointer = 0
-    right_pointer = 0
-    while left_pointer < len(word):
-        if word[right_pointer] in vowels:
-            if word[right_pointer + 1] in vowels: # Null coda, next syllable null onset
-                syllables.append(word[left_pointer:right_pointer + 1] )
-            elif word[right_pointer + 2] in vowels: # Null coda, next syllable non-null onset
-                syllables.append(word[left_pointer:right_pointer + 1] )
-            else: # Non-null coda
-                syllables.append()
-
-
-
-
-
+    while len(word) > 0:
+        temp_syllable = ""
+        if word[0] == "X":
+            break
+        if word[0] in consonants: # Add optional onset
+            temp_syllable += word[0]
+            word = word[1:]
+        temp_syllable += word[0] # Add nucleus
+        word = word[1:]
+        # If neither of the following two cases
+        # Null coda, next syllable null onset (case 1)
+        # Null coda, next syllable non-null onset (case 2)
+        if word[0] not in vowels and word[1] not in vowels:
+            temp_syllable += word[0]
+            word = word[1:]
+        syllables.append(temp_syllable.strip("X") )
+    return syllables
 
 
 class Word:
     def __init__(self, word):
-        self.syllables = []
-        self.num_syllables = 0
+        word = word.lower()
+        self.syllables = decompose_word(word)
+        self.num_syllables = len(self.syllables)
+
+    def __str__(self):
+        return str(self.syllables)
+
+
+if __name__ == '__main__':
+    test_word = Word("testo")
+    print(test_word)
